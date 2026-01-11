19ab8ffcd49ef5089d2263f2d21f99015ca9f7fb
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2026-01-11 15:14:40 +0900

    Migrate from microbundle to rollup

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+import bundleSize from "rollup-plugin-bundle-size";
+
+export default {
+  input: "src/index.js",
+  external: [
+    "@hotwired/stimulus",
+    "@kanety/stimulus-static-actions"
+  ],
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      exports: "auto",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.module.mjs",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.umd.js",
+      format: "umd",
+      name: "StimulusNestedForm",
+      globals: {
+        "@hotwired/stimulus": "Stimulus",
+        "@kanety/stimulus-static-actions": "StimulusStaticActions",
+      },
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".js"],
+      exclude: "node_modules/**",
+    }),
+    terser(),
+    bundleSize()
+  ],
+};
